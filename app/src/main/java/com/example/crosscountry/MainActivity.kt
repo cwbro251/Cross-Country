6a0b59b75dcec8b85c948772433c3de525e1d29a
Curtis Brown <cwbro251@learner.dvusd.org>
2022-05-06 10:26:59 -0700

    Done

--- app/src/main/java/com/example/crosscountry/MainActivity.kt
+++ app/src/main/java/com/example/crosscountry/MainActivity.kt
@@ -2,10 +2,68 @@ package com.example.crosscountry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.crosscountry.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var ite : Int = 1
+        var j1 : Int = 1
+        var a : Int = 0
+
+        binding.team1.setOnClickListener(){
+
+            if ( j1 == 1)
+            {
+                a += ite
+                binding.team1a.text = ite.toString()
+                binding.team1e.text = a.toString()
+                j1++
+                ite++
+
+            }
+            else if (j1 == 2)
+            {
+                a += ite
+                binding.team1b.text = ite.toString()
+                binding.team1e.text = a.toString()
+                j1++
+                ite++
+            }
+            else if (j1 == 3)
+            {
+                a += ite
+                binding.team1c.text = ite.toString()
+                binding.team1e.text = a.toString()
+                ite++
+                j1++
+            }
+            else if (j1 == 4)
+            {
+                a += ite
+                binding.team1d.text = ite.toString()
+                binding.team1e.text = a.toString()
+                j1++
+                ite++
+
+            }
+            else if (j1 == 5)
+            {
+                a += ite
+                binding.team1e.text = ite.toString()
+                j1++
+                ite++
+                binding.team1e.text = a.toString()
+            }
+            else if (j1 > 5)
+            {
+                binding.team1e.text = a.toString()
+            }
+        }
+
     }
 }
